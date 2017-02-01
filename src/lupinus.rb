ef6fda80be8693bcbfc09fd4afef286e1da4c337
Qsaka <949241796@qq.com>
2017-02-01 19:46:46 +0800

    fix bugs

--- src/lupinus.rb
+++ src/lupinus.rb
@@ -26,7 +26,7 @@ class Lupinus
   $WHITESPACE = " \t\r\b\f"
 
   $re_space = Regexp.new("[" + $WHITESPACE + "]*(\n|$)")
-  $re_tag = Regexp.new(format('%s([#^>&{/!]?)\s*(.*?)\s*([}]?)%s', *$DEFAULT_DELIMITERS), Regexp::MULTILINE)
+  $re_tag = Regexp.new(format('%s([;#/!>r{]?)\s*(.*?)\s*([}]?)%s', *$DEFAULT_DELIMITERS), Regexp::MULTILINE)
   $filters = {}
 
   def self.render(*arg, **kwargs)
@@ -40,13 +40,142 @@ class Lupinus
       raise Exception
     end
 
-    inner_render(template, contexts, partials, delimiters)
+    Render::inner_render(template, contexts, partials, delimiters)
   end
 
-  def inner_render(template, contexts, partials={}, delimiters=nil)
-    delimiters = delimiters ? delimiters : $DEFAULT_DELIMITERS
-    parent_token = compiled(template, delimiters)
-    parent_token._render(contexts, partials)
+  class Render
+
+    def self.inner_render(template, contexts, partials={}, delimiters=nil)
+      delimiters = delimiters ? delimiters : $DEFAULT_DELIMITERS
+      parent_token = compiled(template, delimiters)
+      parent_token._render(contexts, partials)
+    end
+
+    def self.standalone?(text, start, end_)
+      # 
+      left = true
+      right = text.match($re_space, end_)
+
+      while (start = start - 1) >= 0
+        if $WHITESPACE.include? text[start]
+          next
+        elsif text[start] == "\n"
+          break
+        else
+          left = false
+          break
+        end
+      end
+
+      if left && right
+        return start + 1, right.end(0)
+      end
+    end
+
+    def self.compiled(template, delimiters=$DEFAULT_DELIMITERS)
+
+      tokens = []
+      index = 0
+      sections = []
+      tokens_stack = []
+
+      root = Root.new "root"
+      root.filters = $filters.clone
+
+      while (m = $re_tag.match(template, index))
+        token = nil
+        last_literal = nil
+        strip_space = false
+
+        if m.begin(0) > index
+          last_literal = Literal.new("str", template[index..m.begin(0)-1], root=root)
+          tokens << last_literal
+        end
+
+        prefix, name, suffix = m.captures()
+
+        if prefix == "r"
+          # {{r name }}
+          # raw string
+          token = Variable.new(name, name, root=root)
+          token.escape = false
+
+        elsif prefix == "" && suffix == ""
+          # {{ name }}
+          token = Variable.new(name, name, root=root)
+          token.escape = true
+
+        elsif prefix == ";"
+          # {{; comment }}
+          token = Comment.new(name, root=root)
+          if sections.length <= 0
+            strip_space = true
+          end
+
+        elsif prefix == ">"
+          token = Partial.new(name, name, root=root)
+          strip_space = true
+          pos = standalone?(template, m.begin(0), m.end(0))
+          if pos
+            token.indent = template[pos[0], m.begin(0)].length
+          end
+
+        elsif prefix == "#" || prefix == "!"
+          sec_name = name.split("|")[0].strip
+          if prefix == "#"
+            token = Section.new(sec_name, name, root=root)
+          else
+            token = Inverted.new(name, name, root=root)
+          end
+          token.delimiter = delimiters
+          tokens << token
+
+          token = nil
+          tokens_stack << tokens
+          tokens = []
+
+          sections << [sec_name, prefix, m.end(0)]
+          strip_space = true
+
+        elsif prefix == "/"
+          tag_name, sec_type, text_end = sections.pop
+          if tag_name != name
+            raise SyntaxError, ""
+          end
+          children = tokens
+          tokens = tokens_stack.pop()
+
+          tokens[-1].text = template[text_end, m.begin(0)]
+          tokens[-1].children = children
+          strip_space = true
+        
+        else
+          raise SyntaxError, "Unkown tag"
+        
+        end
+
+        if token
+          tokens << token
+        end
+
+        index = m.end(0)
+
+        if strip_space
+          pos = standalone?(template, m.begin(0), m.end(0))
+          if pos
+            index = pos[1]
+            if last_literal
+              last_literal.value = last_literal.value.strip($WHITESPACE)
+            end
+          end
+        end
+      end
+
+      tokens << Literal.new("str", template[index..-1])
+
+      root.children = tokens
+      return root
+    end
   end
 
   def self.lookup(var_name, contexts=[], start=0)
@@ -63,157 +192,35 @@ class Lupinus
     return nil
   end
 
-  def standalone?(text, start, end_)
-    # 
-    left = true
-    right = text.match($re_space, end_)
-
-    while (start = start - 1) >= 0
-      if $WHITESPACE.include? text[start]
-        next
-      elsif text[start] == "\n"
-        break
-      else
-        left = false
-        break
-      end
-    end
-
-    if left && right
-      return start + 1, right.end(0)
-    end
-  end
-
-  def compiled(template, delimiters=$DEFAULT_DELIMITERS)
-
-    tokens = []
-    index = 0
-    sections = []
-    tokens_stack = []
-
-    root = Root.new "root"
-    root.filters = $filters.clone
-
-    m = $re_tag.match(template, index)
-    while m
-      token = nil
-      last_literal = nil
-      strip_space = false
-
-      if m.begin(0) > index
-        last_literal = Literal.new("str", template[index..m.begin(0)-1], root=root)
-
-        tokens << last_literal
-      end
-
-      prefix, name, suffix = m.captures()
-
-
-      if prefix == "{" && suffix == "}"
-        token = Variable.new(name, name, root=root)
-      elsif prefix == "" && suffix == ""
-        token = Variable.new(name, name, root=root)
-        token.escape = true
-      elsif suffix != "" && suffix != nil
-        raise SyntaxError, "Invalid token"
-      elsif prefix == "&"
-        token = Variable.new(name, name, root=root)
-      elsif prefix == "!"
-        token = Comment.new(name, root=root)
-        if len(sections) <= 0
-          strip_space = true
-        end
-      elsif prefix == ">"
-        token = Paritial.new(name, name, root=root)
-        strip_space = true
-        pos = standalone?(template, m.begin(0), m.end(0))
-        if pos
-          token.indent = template[pos[0], m.begin(0)].length
-        end
-      elsif prefix == "#" || prefix == "^"
-        sec_name = name.split("|")[0].strip
-        if prefix == "#"
-          token = Section.new(sec_name, name, root=root)
-        else
-          token = Inverted.new(name, name, root=root)
-        end
-        token.delimiter = delimiters
-        tokens << token
-
-        token = nil
-        tokens_stack << tokens
-        tokens = []
-
-        sections << [sec_name, prefix, m.end(0)]
-        strip_space = true
-      elsif prefix == "/"
-        tag_name, sec_type, text_end = sections.pop
-        if tag_name != name
-          raise SyntaxError, "fuck ruby"
-        end
-        children = tokens
-        tokens = tokens_stack.pop()
-
-        tokens[-1].text = template[text_end, m.begin(0)]
-        tokens[-1].children = children
-        strip_space = true
-      else
-        raise SyntaxError, "Unkown tag"
-      end
-
-      if token
-        tokens << token
-      end
-
-      index = m.end(0)
-
-      if strip_space
-        pos = standalone?(template, m.begin(0), m.end(0))
-        if pos
-          index = pos[1]
-          if last_literal
-            last_literal.value = last_literal.value.strip($WHITESPACE)
-          end
-        end
-      end
-      m = $re_tag.match(template, index)
-    end
-    tokens << Literal.new("str", template[index..-1])
-
-    root.children = tokens
-    return root
-  end
-
-
-
   class Token
 
-    attr_accessor :filters, :name, :value, :text, :children, :escape, :delimiter, :indent, :root
+    attr_accessor :filters, :name, :value, :text, :children, \
+                  :escape, :delimiter, :indent, :root
+    
     def initialize(name, value=nil, text="", children=nil, root=nil)
       @name = name
       @value = value
       @text = text
       @children = children
-      @excape = false
+      @escape = false
       @delimiter = nil
       @indent = 0
       @root = root
       @filters = {}
     end
 
-    def _escape(text)
-      rtn = text ? text : $EMPTYSTRING
-
-      if @excape
-        CGI.escapeHTML rtn
+    def escape(text)
+      # escape
+      if @escape
+        CGI.escapeHTML text
       else
-        rtn
+        text
       end
     end
 
     def _look_up(dot_name, contexts)
       list = dot_name.split("|").map(&:strip)
-      dot_name =list[0]
+      dot_name = list[0]
       filters = list[1..-1]
 
       if not dot_name.start_with?(".")
@@ -234,7 +241,6 @@ class Lupinus
           level = level + path.strip(".").split(".").length
         end
       end
-
 
       names = last_path.split(".")
 
@@ -279,7 +285,7 @@ class Lupinus
 
         rtn << child._render(contexts, partials)
       end
-      return rtn.join("")
+      return rtn.join
     end
 
     def _get_str(indent)
@@ -331,7 +337,7 @@ class Lupinus
       @type_string = "L"
     end
     def _render(contexts, partials)
-      return _escape(@value)
+      return escape(@value)
     end
   end
 
@@ -348,7 +354,7 @@ class Lupinus
       if (defined? value) == "method"
         value = inner_render(value(), contexts, partials)
       end
-      return _escape(value)
+      return escape(value)
     end
   end
 
@@ -375,7 +381,7 @@ class Lupinus
         if rtn.length <= 0
           return $EMPTYSTRING
         end
-        return _escape(rtn.join(""))
+        return escape(rtn.join(""))
       elsif (defined? val) == "method"
         new_template = val(@text)
         value = inwner_render(new_template, contexts, partials, @delimiter)
@@ -384,7 +390,7 @@ class Lupinus
         value = _render_children(contexts, partials)
         contexts.pop
       end
-      return _escape(value)
+      return escape(value)
     end
   end
 
@@ -427,7 +433,7 @@ class Lupinus
       partial = partials[@value]
 
       # !!!
-      return inner_render(partial, contexts, partials, @delimiter)
+      return Render::inner_render(partial, contexts, partials, @delimiter)
     end
   end
 end
@@ -440,22 +446,24 @@ end
 # test
 require "json"
 template_text = <<~EOF
-<h1>{{header}}</h1>
-{{#bug}}
-{{/bug}}
+<h1>Today{{; ignore me }}.</h1>
+<h1>{{ header }}</h1>
+{{# bug }}
+{{/ bug }}
 
-{{#items}}
-  {{#first}}
+{{# items }}
+  {{# first }}
     <li><strong>{{name}}</strong></li>
-  {{/first}}
-  {{#link}}
+  {{/ first }}
+  {{# link }}
     <li><a href="{{url}}">{{name}}</a></li>
-  {{/link}}
-{{/items}}
+  {{/ link }}
+{{/ items }}
 
-{{#empty}}
+{{! empty }}
   <p>The list is empty.</p>
-{{/empty}}
+{{/ empty }}
+{{> hello }}
 EOF
 
 context_text = <<~EOF
@@ -466,46 +474,31 @@ context_text = <<~EOF
       {"name": "green", "link": true, "url": "#Green"},
       {"name": "blue", "link": true, "url": "#Blue"}
   ],
-  "empty": false
+  "empty": false,
+  "hello": "{{ name }}",
+  "name" : "miziha"
 }
 EOF
 
 context = JSON.parse(context_text)
-puts Lupinus.render(template_text, context) 
+puts Lupinus.render(template_text, context,{'hello'=>'{{name}}'}) 
 
 
 
-template_text = <<~EOF
-{{#repo}}
-  <b>{{name}}</b>
-{{/repo}}
-EOF
+# template_text = <<~EOF
+# {{#repo}}
+#   <b></b>
+# {{/repo}}
+# {{!repo}}
+#   No repos :(
+# {{/repo}}
+# EOF
 
-context_text = <<~EOF
- {
-  "repo": [
-    { "name": "resque" },
-    { "name": "hub" },
-    { "name": "rip" }
-  ]
-}
-EOF
-context = JSON.parse(context_text)
-puts Lupinus.render(template_text, context) 
-template_text = <<~EOF
-{{#repo}}
-  <b>{{name}}</b>
-{{/repo}}
-{{^repo}}
-  No repos :(
-{{/repo}}
-EOF
-
-context_text = <<~EOF
-{
-  "repo": []
-}
-EOF
-context = JSON.parse(context_text)
-puts Lupinus.render(template_text, context) 
+# context_text = <<~EOF
+# {
+#   "repo": []
+# }
+# EOF
+# context = JSON.parse(context_text)
+# puts Lupinus.render(template_text, context) 
 
